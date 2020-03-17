bb1dd639bb5de26c581ea3787d58fd5c0ce39048
annisokay97 <annisokay97@gmail.com>
2020-03-17 15:00:01 +0200

    refactor: Change getting by key in object to switch-case syntax in avatarSize function

--- packages/ui/src/components/Avatar/styles/inject.js
+++ packages/ui/src/components/Avatar/styles/inject.js
@@ -5,36 +5,32 @@ const background = (background = 'var(--g-primary)') => `background: ${backgroun
 
 const color = (color = 'var(--c-lightest)') => `color: ${color}`;
 
-const avatarSize = (size = 'medium') => {
-  const avatarSize = {
-    small: css`
-      width: 1.75rem;
-      height: 1.75rem;
-      border-radius: 1.75rem;
+const avatarSize = size => {
+  switch (size) {
+    case 'small':
+      return css`
+        width: 1.75rem;
+        height: 1.75rem;
+        border-radius: 1.75rem;
 
-      ${Mixin.Font.bodySmall()};
-    `,
-    medium: css`
-      width: 2.75rem;
-      height: 2.75rem;
-      border-radius: 2.75rem;
+        ${Mixin.Font.bodySmall()};
+      `;
+    case 'large':
+      return css`
+        width: 4rem;
+        height: 4rem;
+        border-radius: 4rem;
 
-      ${Mixin.Font.h5()};
-    `,
-    large: css`
-      width: 4rem;
-      height: 4rem;
-      border-radius: 4rem;
+        ${Mixin.Font.h2()};
+      `;
+    default:
+      return css`
+        width: 2.75rem;
+        height: 2.75rem;
+        border-radius: 2.75rem;
 
-      ${Mixin.Font.h2()};
-    `,
-  };
-
-  if (avatarSize[size]) {
-    return avatarSize[size];
-  } else {
-    console.error(`Invalid Avatar size. Use ${Object.keys(avatarSize)}`);
-    return avatarSize.medium;
+        ${Mixin.Font.h5()};
+      `;
   }
 };
 
