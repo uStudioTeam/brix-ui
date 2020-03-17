1ae1b1979d8182e900585ae365e4083995804717
Dmytro Vasylkivskyi <drizzer14@gmail.com>
2020-03-17 13:15:27 +0200

    feat: Add StyledComponent decorator

---
+++ packages/ui/src/utils/styles/styled-component.js
@@ -0,0 +1,10 @@
+export const StyledComponent = ({ displayName }) => Component => {
+  Component.displayName = displayName;
+  Component.attrs = [
+    props => ({
+      className: `${props.classNames?.[displayName] || ''} ${props?.className || ''}`.trim(),
+    }),
+  ];
+
+  return Component;
+};
