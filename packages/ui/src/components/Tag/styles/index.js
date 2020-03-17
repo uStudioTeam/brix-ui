2e608137efac57aa02dc1b687edb97516a6a1647
Hanna <23137619+annisokay97@users.noreply.github.com>
2020-03-17 17:43:48 +0200

    refactor(Tag): Refactor Tag component (#11)
    * feat: Add StyledComponents decorator
    
    * refactor: Move Tag styles to separated folder
    
    * fix: Fix appearance props

---
+++ packages/ui/src/components/Tag/styles/index.js
@@ -0,0 +1,18 @@
+import styled, { css } from 'styled-components';
+
+import { Mixin } from '../../../theme';
+import { StyledComponents } from '../../../utils/styles/styled-component';
+import { inject } from './inject';
+
+const Tag = styled.span(
+  ({ appearance }) => css`
+    ${Mixin.Font.bodySmall()};
+
+    padding: 3px var(--i-medium) var(--i-small);
+    border-radius: var(--border-radius);
+    ${inject.background(appearance?.background)};
+    ${inject.color(appearance?.color)};
+  `
+);
+
+export const Styled = StyledComponents({ Tag });
