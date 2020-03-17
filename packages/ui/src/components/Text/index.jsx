dbae6b0464fb494e5081e1a3b98bcfe8a839926a
Dmytro Vasylkivskyi <drizzer14@gmail.com>
2020-03-17 12:23:36 +0200

    refactor: Move renderElement function to textUtils

--- packages/ui/src/components/Text/index.jsx
+++ packages/ui/src/components/Text/index.jsx
@@ -4,44 +4,23 @@ import PropTypes from 'prop-types';
 import { classNames, common } from '../../utils';
 
 import { Styled } from './styles';
-
-function renderElement({ variant, appearance }) {
-  if (appearance === 'bold') {
-    return 'strong';
-  }
-
-  if (appearance === 'italic') {
-    return 'em';
-  }
-
-  switch (variant) {
-    case 'code':
-    case 'small':
-    case 'h1':
-    case 'h2':
-    case 'h3':
-    case 'h4':
-    case 'h5':
-    case 'h6':
-      return variant;
-    case 'caption':
-      return 'small';
-    case 'article':
-    case 'body':
-      return 'p';
-    case 'span':
-    default:
-      return 'span';
-  }
-}
+import { textUtils } from './utils';
 
 const Text = forwardRef(function Text(
-  { children = '', variant = 'body', align = 'left', appearance = 'regular', className = '', classNames, ...htmlAttributes },
+  {
+    children = '',
+    variant = 'body',
+    align = 'left',
+    appearance = 'regular',
+    className = '',
+    classNames,
+    ...htmlAttributes
+  },
   ref
 ) {
   return (
     <Styled.Text
-      as={renderElement({ variant, appearance })}
+      as={textUtils.renderElement({ variant, appearance })}
       ref={ref}
       variant={variant}
       align={align}
