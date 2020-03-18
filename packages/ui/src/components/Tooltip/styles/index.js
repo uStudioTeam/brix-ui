e6f75f9a82e1644abb28664e5f6070f35a09a0a2
annisokay97 <annisokay97@gmail.com>
2020-03-18 11:43:57 +0200

    refactor: Fix Tooltip styles

---
+++ packages/ui/src/components/Tooltip/styles/index.js
@@ -0,0 +1,74 @@
+import styled, { css } from 'styled-components';
+import { Mixin } from '../../../theme';
+import { StyledComponents } from '../../../utils/styles/styled-component';
+import { inject } from './inject';
+
+const Tooltip = styled.span(
+  ({ position }) => css`
+  position: absolute;
+  ${inject.indent(position)};
+
+  display: block;
+  width: max-content;
+  padding: var(--i-small) var(--i-medium);
+  opacity: 0;
+  z-index: var(--l-bottom);
+
+  background-color: var(--c-light);
+  border-radius: var(--border-radius);
+  box-shadow: var(--s-light);
+
+  transform: ${inject.translateByPosition(position)};
+  transition: opacity var(--transition);
+  transition-delay: 5ms;
+
+  pointer-events: none;
+
+  &:before {
+    content: '';
+
+    position: absolute;
+    ${inject.reversePosition(position)}: -0.5rem;
+    // center by opposite axis
+    ${Object.keys(inject.sidePositions(position))[0]}: 50%;
+
+    // triangle from borders
+    border-${position}: var(--i-medium) solid var(--c-light);
+    ${inject.sideBordersByPosition(position)};
+
+    ${inject.compensateCentring(position)}
+
+  };
+}`
+);
+
+const TooltipContainer = styled.span`
+  position: relative;
+  border-bottom: 1px dotted var(--c-dark);
+  cursor: default;
+
+  ${Mixin.Device.mobile(css`
+    &:focus,
+    &:focus-within {
+      cursor: help;
+
+      ${Tooltip} {
+        pointer-events: auto;
+        opacity: 1;
+        user-select: none;
+      }
+    }
+  `)}
+  ${Mixin.Device.desktop(css`
+    &:hover {
+      cursor: help;
+
+      ${Tooltip} {
+        pointer-events: auto;
+        opacity: 1;
+        user-select: none;
+      }
+    }
+  `)}
+`;
+export const Styled = StyledComponents({ TooltipContainer, Tooltip });
---
+++ packages/ui/src/components/Tooltip/styles/inject.js
@@ -0,0 +1,64 @@
+import { css } from 'styled-components';
+
+function reversePosition(position) {
+  return {
+    top: 'bottom',
+    right: 'left',
+    bottom: 'top',
+    left: 'right',
+  }[position];
+}
+
+function sidePositions(position) {
+  switch (position) {
+    case 'top':
+    case 'bottom':
+      return { left: 'left', right: 'right' };
+    case 'left':
+    case 'right':
+    default:
+      return { top: 'top', bottom: 'bottom' };
+  }
+}
+
+function translateByPosition(position) {
+  return {
+    top: css`translate(-50%, calc(-100% - 0.25rem))`,
+    right: css`translate(calc(100% + 0.25rem), -50%)`,
+    bottom: css`translate(-50%, calc(100% + 0.25rem))`,
+    left: css`translate(calc(-100% - 0.25rem), -50%)`,
+  }[position];
+}
+
+function sideBordersByPosition(position) {
+  return Object.keys(sidePositions(reversePosition(position))).reduce(
+    (map, pos) => `
+        ${map}
+        border-${pos}: 0.5rem solid transparent;
+      `,
+    ''
+  );
+}
+
+function indent(position) {
+  if (position === 'left' || position === 'right') {
+    // move sideways
+    return `${position}: -0.25rem; top: 50%`;
+  }
+  if (position === 'top' || position === 'bottom') {
+    // center horizontally
+    return `${position}: -0.25rem; left: 50%;`;
+  }
+}
+function compensateCentring(position) {
+  return `transform: translate${position === 'left' || position === 'right' ? 'Y' : 'X'}(-50%)`;
+}
+
+export const inject = {
+  reversePosition,
+  sidePositions,
+  translateByPosition,
+  sideBordersByPosition,
+  indent,
+  compensateCentring,
+};
