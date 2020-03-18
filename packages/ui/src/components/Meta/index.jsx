070ba868933b8b5d0289421cc28206000e3eef9e
annisokay97 <annisokay97@gmail.com>
2020-03-18 10:55:10 +0200

    refactor: Refactor Meta markup

--- packages/ui/src/components/Meta/index.jsx
+++ packages/ui/src/components/Meta/index.jsx
@@ -1,33 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Text from '../Text';
-
 import { classNames } from '../../utils';
 
 import { Styled } from './styles';
 
-function renderValue(metaProps) {
-  const { href, ...props } = metaProps;
-  const { variant, value, classNames } = props;
-
-  return href ? (
-    <Styled.Link href={href} className={classNames?.Link || ''}>
-      {renderValue(props)}
-    </Styled.Link>
-  ) : (
-    <Text variant={variant === 'small' ? 'body' : 'h4'}>{value}</Text>
-  );
-}
-
-const Meta = ({ variant = 'small', title, value, href, classNames, className = '' }) => (
+const Meta = ({ variant = 'small', title, value, classNames, className = '' }) => (
   <Styled.Meta classNames={classNames} className={className}>
     {title && (
-      <Styled.Title variant={variant === 'small' ? 'caption' : 'h6'} classNames={classNames}>
+      <Styled.Title variant={variant} classNames={classNames}>
         {title}
       </Styled.Title>
     )}
-    {renderValue({ variant, value, href, classNames })}
+    <Styled.Value variant={variant} className={classNames}>
+      {value}
+    </Styled.Value>
   </Styled.Meta>
 );
 
@@ -35,9 +22,8 @@ Meta.displayName = 'Meta';
 
 Meta.propTypes = {
   variant: PropTypes.oneOf(['small', 'large']),
-  title: PropTypes.string,
-  value: PropTypes.string.isRequired,
-  href: PropTypes.string,
+  title: PropTypes.node,
+  value: PropTypes.node.isRequired,
   ...classNames(Object.keys(Styled)),
 };
 
