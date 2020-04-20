069e7130c949aee5fe2ba26fb469162b7ba9c3d7
hanna <annisokay97@gmail.com>
2020-04-20 16:36:10 +0300

    feat(Theme): add white and black colors

--- packages/ustudio-ui/src/components/Badge/styles/inject.js
+++ packages/ustudio-ui/src/components/Badge/styles/inject.js
@@ -2,7 +2,7 @@ import { css } from 'styled-components';
 
 const background = (background = 'var(--g-primary)') => `background: ${background}`;
 
-const color = (color = 'var(--c-lightest)') => `color: ${color}`;
+const color = (color = 'var(--c-white)') => `color: ${color}`;
 
 const disabledStyles = isDisabled => {
   return isDisabled
