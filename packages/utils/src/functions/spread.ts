88b1ab0957c76a388ed701c1ec2a8e4a37283810
drizzer14 <drizzer14@gmail.com>
2020-07-31 23:29:26 +0300

    refactor(grid-system): Refactor imports and configs

---
+++ packages/utils/src/functions/spread.ts
@@ -0,0 +1,13 @@
+import type { Values } from '@ustudio-ui/utils/types';
+
+export const spread = <
+  _O extends Record<string, unknown> | undefined,
+  O extends Exclude<_O, undefined>,
+  R extends Record<keyof O, Values<O>>
+>(
+  ...objects: _O[]
+): R => {
+  return objects.reduce((spreadObject, object) => {
+    return Object.assign(spreadObject, object || {});
+  }, {} as R);
+};
