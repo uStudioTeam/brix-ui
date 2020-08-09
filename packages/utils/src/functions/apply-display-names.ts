23ef23a23705986017eff1862ffb98e75fd9bced
drizzer14 <drizzer14@gmail.com>
2020-08-09 19:18:23 +0300

    feat: Export all styled components through `apply-display-name` function
    Applies `displayName` property

---
+++ packages/utils/src/functions/apply-display-names.ts
@@ -0,0 +1,10 @@
+import { StyledComponent } from 'styled-components';
+
+export const applyDisplayNames = <S extends Record<string, StyledComponent<any, any>>>(styledObject: S): S => {
+  return Object.keys(styledObject).reduce((newStyledObject, styledComponentName) => {
+    const styledComponent = styledObject[styledComponentName];
+    styledComponent.displayName = `Styled${styledComponentName}`;
+
+    return Object.assign(newStyledObject, { [styledComponentName]: styledComponent });
+  }, {} as S);
+};
