f7261a2043d3a5c42f564f17d84b1cd1ce1a75b3
drizzer14 <drizzer14@gmail.com>
2020-08-11 22:54:02 +0300

    fix(theme): Remove inappropriate import

--- packages/theme/src/typography/index.ts
+++ packages/theme/src/typography/index.ts
@@ -1,3 +1,3 @@
-export type { FontsMap, FontsFacesMap, FontsCssMap, Typography } from './entity';
+export type { FontsMap, FontsFacesMap, FontsCssMap } from './entity';
 export * from './typography.mixin';
 export { default } from './typography.component';
