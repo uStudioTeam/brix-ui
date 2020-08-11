7964585df0d60f8475c7f95d0bb48dbf842d43f0
drizzer14 <drizzer14@gmail.com>
2020-08-11 23:22:17 +0300

    feat(grid): Add story
    WIP

--- packages/grid/stories/grid.story.tsx
+++ packages/grid/stories/grid.story.tsx
@@ -1,3 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Flex from '@ustudio-ui/core/flex';
+
+import { Grid, Cell } from '@ustudio-ui/grid';
+
 export default {
   title: 'Data/Grid',
+};
+
+const Box = styled(Flex).attrs(() => ({
+  padding: '2rem',
+  align: 'center',
+}))`
+  color: var(--c-base-s);
+  background-color: var(--c-faint-w);
+
+  border: 1px solid var(--c-faint-w-u);
+`;
+
+export const Basic = () => {
+  return (
+    <Grid gap="1rem" sm={{ gap: '2rem' }}>
+      <Cell>
+        <Box>1</Box>
+      </Cell>
+
+      <Cell>
+        <Box>2</Box>
+      </Cell>
+
+      <Cell>
+        <Box>3</Box>
+      </Cell>
+    </Grid>
+  );
 };
