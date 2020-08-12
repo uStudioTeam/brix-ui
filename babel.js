37ecb317c4d9045ca4ced238060b2ce5c2a7cc5f
drizzer14 <drizzer14@gmail.com>
2020-08-12 11:04:04 +0300

    chore: Add babel-plugin-inline-react-svg

--- babel.js
+++ babel.js
@@ -1,11 +1,12 @@
 const styledComponentsConfig = ['styled-components', { displayName: true, preprocess: false }];
 const reactConfig = '@babel/preset-react';
+const inlineSvgConfig = 'babel-plugin-inline-react-svg';
 
 const withConfig = (shouldUse, config) => (shouldUse ? [config] : []);
 
-const babelConfig = ({ styledComponents, react }) => ({
-  presets: [withConfig(react, reactConfig), '@babel/preset-typescript'],
-  plugins: [...withConfig(styledComponents, styledComponentsConfig)],
+const babelConfig = ({ styledComponents, react, inlineSvg }) => ({
+  presets: [...withConfig(react, reactConfig), '@babel/preset-typescript'],
+  plugins: [...withConfig(styledComponents, styledComponentsConfig), ...withConfig(inlineSvg, inlineSvgConfig)],
 });
 
 module.exports = babelConfig;
--- packages/core/.babelrc.js
+++ packages/core/.babelrc.js
@@ -1,1 +1,1 @@
-module.exports = require('../../babel')({ styledComponents: true, react: true });
+module.exports = require('../../babel')({ styledComponents: true, react: true, inlineSvg: true });
