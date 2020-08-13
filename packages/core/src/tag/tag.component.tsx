b11642a24c6ab5b2771e4176bf59ba586bd7e00d
hanna <annisokay97@gmail.com>
2020-08-13 17:02:27 +0300

    feat(Tag): Add tag component

---
+++ packages/core/src/tag/tag.component.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { intrinsicComponent } from '@ustudio-ui/utils/functions';
+
+import type { TagProps } from './tag.props';
+import Styled from './tag.styles';
+
+const Tag = intrinsicComponent<TagProps>(function Tag(
+  { children, color, backgroundColor, onClose, closeIcon, ...props },
+  ref
+) {
+  return (
+    <Styled.Tag ref={ref} color={color} backgroundColor={backgroundColor} isInline {...props}>
+      <Styled.Content>{children}</Styled.Content>
+
+      {onClose && (
+        <Styled.CloseContainer onClick={onClose}>
+          {closeIcon ? closeIcon({ onClose }) : <Styled.CloseIcon />}
+        </Styled.CloseContainer>
+      )}
+    </Styled.Tag>
+  );
+});
+
+export default Tag;
---
+++ packages/core/stories/tag.story.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Tag, { TagProps } from '@ustudio-ui/core/tag';
+
+export default {
+  title: 'Data/Tag',
+  component: Tag,
+
+  argTypes: {
+    backgroundColor: { control: 'text' },
+    color: { control: 'text' },
+    onClose: { control: 'boolean', description: 'When true, component has onClose function', type: 'function' },
+    closeIcon: {},
+  },
+};
+
+export const Basic = ({ onClose, ...props }: TagProps) => {
+  const handleClose = () => console.log('Handle close');
+
+  return (
+    <Tag {...props} onClose={onClose ? handleClose : undefined}>
+      Tag text
+    </Tag>
+  );
+};
