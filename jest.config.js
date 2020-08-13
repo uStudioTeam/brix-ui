57648cad5ce0b705420fa06d24241f29af992b3a
drizzer14 <drizzer14@gmail.com>
2020-08-13 14:40:53 +0300

    chore: Create separate jest.config files for modules

---
+++ jest.config.js
@@ -0,0 +1,32 @@
+const tsconfig = require('./tsconfig.json');
+const { paths } = tsconfig.compilerOptions;
+
+const moduleNameMapper = require('tsconfig-paths-jest')(tsconfig);
+
+module.exports = {
+  testEnvironment: 'jsdom',
+  rootDir: './',
+  roots: Object.values(paths)
+    .flatMap(([path]) => path.replace(/\/\*/, ''))
+    .filter((path) => !/index.ts$/.test(path)),
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '.*\\.spec.tsx?$',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -250,
+    },
+  },
+  collectCoverageFrom: ['**/*.ts', '**/*.tsx'],
+  clearMocks: true,
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  moduleDirectories: ['node_modules', '**/node_modules', '**/src'],
+  moduleNameMapper,
+  testPathIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/node_modules/'],
+};
