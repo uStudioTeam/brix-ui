23051edfdecdd3cd127a815e2a6888ce4d81ed57
hanna <annisokay97@gmail.com>
2020-08-14 14:38:54 +0300

    refactor(Tag): Add code alignment

--- packages/core/stories/tag.story.tsx
+++ packages/core/stories/tag.story.tsx
@@ -9,7 +9,12 @@ export default {
   argTypes: {
     backgroundColor: { control: 'text' },
     color: { control: 'text' },
-    onClose: { control: 'boolean', description: 'Generate `closeIcon`, when passed `onClose` prop. When control is true, component has onClose function', type: 'function' },
+    onClose: {
+      control: 'boolean',
+      description:
+        'Generate `closeIcon`, when passed `onClose` prop. When control is true, component has onClose function',
+      type: 'function',
+    },
     closeIcon: {},
   },
 };
