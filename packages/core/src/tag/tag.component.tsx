c8ee6410a572cf83f68e884148230655efcce9c4
hanna <annisokay97@gmail.com>
2020-08-18 14:44:19 +0300

    fix(Tag): Refactor tag

--- packages/core/src/tag/tag.component.tsx
+++ packages/core/src/tag/tag.component.tsx
@@ -5,12 +5,12 @@ import { intrinsicComponent } from '@ustudio-ui/utils/functions';
 import type { TagProps } from './tag.props';
 import Styled from './tag.styles';
 
-const Tag = intrinsicComponent<TagProps>(function Tag(
+const Tag = intrinsicComponent<TagProps, HTMLDivElement>(function Tag(
   { children, color, backgroundColor, onClose, closeIcon, ...props },
   ref
 ) {
   return (
-    <Styled.Tag ref={ref} $color={color} $backgroundColor={backgroundColor} isInline {...props}>
+    <Styled.Tag ref={ref} $color={color} $backgroundColor={backgroundColor} {...props}>
       <Styled.Content>{children}</Styled.Content>
 
       {onClose && <Styled.CloseContainer onClick={onClose}>{closeIcon || <Styled.CloseIcon />}</Styled.CloseContainer>}
