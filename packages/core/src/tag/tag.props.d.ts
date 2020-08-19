59a9e212218f6a35b23f81203adbf303f74a89be
hanna <annisokay97@gmail.com>
2020-08-19 10:26:31 +0300

    fix(Tag): Fix extends type

--- packages/core/src/tag/tag.props.d.ts
+++ packages/core/src/tag/tag.props.d.ts
@@ -2,7 +2,7 @@ import { ReactElement } from 'react';
 
 import type { IntrinsicComponent } from '@ustudio-ui/types/component';
 
-export interface TagProps extends IntrinsicComponent {
+export interface TagProps extends IntrinsicComponent<HTMLDivElement> {
   backgroundColor?: string;
   color?: string;
 
