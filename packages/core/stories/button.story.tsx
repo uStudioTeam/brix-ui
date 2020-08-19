6644c1589d569da65a6ab43755e5ff02b6b897ee
natalia-lypovyk <cardinnata@gmail.com>
2020-08-19 16:50:56 +0300

    refactor: add description to args

--- packages/core/stories/button.story.tsx
+++ packages/core/stories/button.story.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Meta, Story } from '@storybook/react';
 
-import Button from '@ustudio-ui/core/button';
+import Button, { ButtonProps } from '@ustudio-ui/core/button';
 
 export default {
   title: 'Data/Button',
@@ -12,27 +13,30 @@ export default {
         type: 'inline-radio',
         options: ['base', 'accent', 'critical', 'success'],
       },
+      description: 'Variant of the button color',
     },
     appearance: {
       control: {
         type: 'inline-radio',
         options: ['contained', 'outlined', 'faint', 'text'],
       },
+      description: 'Variant of the button look',
     },
     isDisabled: {
       control: 'boolean',
       description: 'Disables button',
       type: 'function',
     },
-    args: {
-      intent: 'base',
-      appearance: 'contained',
-      isDisabled: false,
-    },
   },
-};
 
-export const Basic = (args) => {
+  args: {
+    intent: 'base',
+    appearance: 'contained',
+    isDisabled: false,
+  },
+} as Meta;
+
+export const Basic: Story<ButtonProps> = (args) => {
   return (
     <Button {...args}>Click</Button>
   );
