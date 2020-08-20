13c1bf4ce7b71b7214339e7c2f50afa4dc6e005a
natalia-lypovyk <cardinnata@gmail.com>
2020-08-20 09:49:50 +0300

    refactor: remove desctructured button props

--- packages/core/src/button/button.component.tsx
+++ packages/core/src/button/button.component.tsx
@@ -3,21 +3,14 @@ import React from 'react';
 import { intrinsicComponent } from '@ustudio-ui/utils/functions';
 
 import { ButtonProps } from './button.props';
-
 import Styled from './button.styles';
 
 const Button = intrinsicComponent<ButtonProps, HTMLButtonElement>(function Button(
-  { children, intent, appearance, isDisabled, ...props },
+  { children, ...props },
   ref
 ) {
   return (
-    <Styled.Button
-      ref={ref}
-      intent={intent}
-      appearance={appearance}
-      isDisabled={isDisabled}
-      {...props}
-    >
+    <Styled.Button ref={ref} {...props}>
       {children}
     </Styled.Button>
   );
