59aa435c53d8f5fe994c4cb7a456d39948f3a9f6
drizzer14 <drizzer14@gmail.com>
2020-08-28 13:47:22 +0300

    feat(Button): Try a different approach for button interactions

--- packages/core/src/button/button.mixin.ts
+++ packages/core/src/button/button.mixin.ts
@@ -6,8 +6,12 @@ import { ColorTransformer } from '@ustudio-ui/theme/palette';
 import { ColorSpace, Color } from '@ustudio-ui/types/palette';
 import { Values } from '@ustudio-ui/utils/types';
 
-const applyIntentStyle = (color: string, baseStyles: string, intentStyles: string): string => {
-  if (color === 'base') {
+type Intent = NonNullable<ButtonProps['intent']>;
+
+type IntentMixin = (intent: Intent) => FlattenSimpleInterpolation;
+
+const applyIntentStyle = <S>(intent: Intent, baseStyles: S, intentStyles: S): S => {
+  if (intent === 'base') {
     return baseStyles;
   }
 
@@ -17,7 +21,11 @@ const applyIntentStyle = (color: string, baseStyles: string, intentStyles: strin
 const applyShadow = (color: string, alpha: number) => ({ theme }: WithTheme): FlattenSimpleInterpolation => {
   return css`0 2px 8px
       ${ColorTransformer.toHsla(
-        ColorTransformer.toTuple(theme.palette[color as Values<typeof Color>], ColorSpace.HSL) as [number, number, number],
+        ColorTransformer.toTuple(theme.palette[color as Values<typeof Color>], ColorSpace.HSL) as [
+          number,
+          number,
+          number
+        ],
         alpha
       )};
   `;
@@ -25,8 +33,6 @@ const applyShadow = (color: string, alpha: number) => ({ theme }: WithTheme): Fl
 
 export const disabledButtonMixin = {
   contained: css`
-    border: 1px solid transparent;
-
     background-color: var(--c-faint-w-u);
 
     color: var(--c-faint-s);
@@ -37,26 +43,24 @@ export const disabledButtonMixin = {
     color: var(--c-faint-s);
   `,
   text: css`
-    border: 1px solid transparent;
-
     color: var(--c-faint-s);
   `,
   faint: css`
-    border: 1px solid transparent;
-
-    background-color: var(--c-faint-w-u);
+    background-color: var(--c-faint-w);
 
     color: var(--c-faint-s);
   `,
 };
 
-const containedButton = (intent: NonNullable<ButtonProps['intent']>) =>
-  css`
-    border: 1px solid transparent;
-
-    background-color: ${`var(--c-${intent}-s)`};
-
+const containedButtonMixin: IntentMixin = (intent) => {
+  return css`
     color: var(--c-base-w);
+
+    &,
+    &:active:focus,
+    &:hover:focus {
+      background-color: ${`var(--c-${intent}-s)`};
+    }
 
     &:hover {
       box-shadow: ${applyShadow(`${intent}-s`, 0.4)};
@@ -67,19 +71,21 @@ const containedButton = (intent: NonNullable<ButtonProps['intent']>) =>
     }
 
     &:active {
-      background-color: ${`var(--c-${intent}-s-d)`};
-      box-shadow: ${applyShadow(`${intent}-s`, 0.3)};
-
-      color: ${applyIntentStyle(intent, `var(--c-faint-s)`, `var(--c-${intent}-w-u)`)};
+      box-shadow: ${applyShadow(`${intent}-s`, 0.25)};
     }
   ` as FlattenSimpleInterpolation;
+};
 
-const outlinedButton = (intent: NonNullable<ButtonProps['intent']>) =>
-  css`
-    border: 1px solid ${`var(--c-${intent}-s)`};
+const outlinedButtonMixin: IntentMixin = (intent) => {
+  return css`
     background-color: var(--c-base-w);
 
-    color: ${`var(--c-${intent}-s)`};
+    &,
+    &:active:focus,
+    &:hover:focus {
+      border: 1px solid ${`var(--c-${intent}-s)`};
+      color: ${`var(--c-${intent}-s)`};
+    }
 
     &:hover {
       box-shadow: ${applyShadow(`${intent}-s`, 0.25)};
@@ -92,43 +98,46 @@ const outlinedButton = (intent: NonNullable<ButtonProps['intent']>) =>
     }
 
     &:active {
-      border: 1px solid ${applyIntentStyle(intent, 'var(--c-faint-s)', `var(--c-${intent}-w-u)`)};
-
       box-shadow: ${applyShadow(`${intent}-s`, 0.15)};
-
-      color: ${`var(--c-${intent}-s-d)`};
     }
   ` as FlattenSimpleInterpolation;
+};
 
-const faintButton = (intent: NonNullable<ButtonProps['intent']>) =>
-  css`
-    border: 1px solid transparent;
-
-    background-color: ${applyIntentStyle(intent, `var(--c-faint-w)`, `var(--c-${intent}-w-d)`)};
-
+const faintButtonMixin: IntentMixin = (intent) => {
+  return css`
     color: ${`var(--c-${intent}-s)`};
 
+    &,
+    &:active:focus,
+    &:hover:focus {
+      background-color: ${applyIntentStyle(intent, `var(--c-faint-w)`, `var(--c-${intent}-w-d)`)};
+    }
+
     &:hover {
-      box-shadow: ${applyShadow(`${intent}-s`, Number(applyIntentStyle(intent, '0.1', '0.15')))};
+      box-shadow: ${applyShadow(`${intent}-s`, Number(applyIntentStyle(intent, 0.1, 0.125)))};
     }
 
     &:focus {
-      background-color: ${applyIntentStyle(intent, `var(--c-faint-w-u)`, `var(--c-${intent}-w)`)};
+      background-color: ${applyIntentStyle(intent, `var(--c-faint-s-d)`, `var(--c-${intent}-w)`)};
     }
 
     &:active {
-      background-color: ${applyIntentStyle(intent, 'var(--c-faint-w-u)', `var(--c-${intent}-w)`)};
-      box-shadow: ${applyShadow(`${intent}-s`, Number(applyIntentStyle(intent, '0.075', '0.15')))};
+      box-shadow: ${applyShadow(`${intent}-s`, 0.075)};
 
-      color: ${`var(--c-${intent}-s-d)`};
+      transform: scale(0.99);
     }
   ` as FlattenSimpleInterpolation;
+};
 
-const textButton = (intent: NonNullable<ButtonProps['intent']>) =>
-  css`
-    border: 1px solid transparent;
-
+const textButtonMixin: IntentMixin = (intent) => {
+  return css`
     color: ${`var(--c-${intent}-s)`};
+
+    &,
+    &:active:focus,
+    &:hover:focus {
+      color: ${`var(--c-${intent}-s)`};
+    }
 
     &:hover {
       background-color: ${applyIntentStyle(intent, `var(--c-faint-w)`, `var(--c-${intent}-w-d)`)};
@@ -143,26 +152,27 @@ const textButton = (intent: NonNullable<ButtonProps['intent']>) =>
     &:active {
       background-color: ${applyIntentStyle(intent, 'var(--c-faint-w)', `var(--c-${intent}-w-d)`)};
 
-      color: ${`var(--c-${intent}-s-d)`};
+      text-shadow: ${applyShadow(`${intent}-s`, 0.25)};
 
-      text-shadow: ${applyShadow(`${intent}-s`, 0.5)};
+      transform: scale(0.99);
     }
   ` as FlattenSimpleInterpolation;
+};
 
-const applyStyle = (
-  mixin: (intent: NonNullable<ButtonProps['intent']>) => FlattenSimpleInterpolation
-): Record<Exclude<ButtonProps['intent'], undefined>, FlattenSimpleInterpolation> => {
-  return ['base', 'accent', 'critical', 'success'].reduce((accumulator, key) => {
-    return Object.assign(accumulator, { [key]: mixin(key as NonNullable<ButtonProps['intent']>) });
-  }, {} as Record<Exclude<ButtonProps['intent'], undefined>, FlattenSimpleInterpolation>);
+const mapIntentStyles = (
+  mixin: (intent: Intent) => FlattenSimpleInterpolation
+): Record<Intent, FlattenSimpleInterpolation> => {
+  return (['base', 'accent', 'critical', 'success'] as Intent[]).reduce((accumulator, key) => {
+    return Object.assign(accumulator, { [key]: mixin(key) });
+  }, {} as Record<Intent, FlattenSimpleInterpolation>);
 };
 
 export const buttonMixin: Record<
   Exclude<ButtonProps['appearance'], undefined>,
-  Record<Exclude<ButtonProps['intent'], undefined>, FlattenSimpleInterpolation>
+  Record<Intent, FlattenSimpleInterpolation>
 > = {
-  contained: applyStyle(containedButton),
-  outlined: applyStyle(outlinedButton),
-  faint: applyStyle(faintButton),
-  text: applyStyle(textButton),
+  contained: mapIntentStyles(containedButtonMixin),
+  outlined: mapIntentStyles(outlinedButtonMixin),
+  faint: mapIntentStyles(faintButtonMixin),
+  text: mapIntentStyles(textButtonMixin),
 };
