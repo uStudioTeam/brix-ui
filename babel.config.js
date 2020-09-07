070eb01c93d8127c1acf78109b41ef6ba00f7dd2
drizzer14 <drizzer14@gmail.com>
2020-09-07 12:13:11 +0300

    chore: Update testing configs

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,8 @@
-module.exports = require('./babel')({ styledComponents: true, react: true });
+module.exports = require('./babel')({
+  styledComponents: true,
+  react: true,
+  env: true,
+  inlineSvg: true,
+  polished: true,
+  runtime: true,
+});
