cb11ce0ff7fccef7088f9fc9c9ca9c615a8ab2fb
drizzer14 <drizzer14@gmail.com>
2020-09-07 17:26:08 +0300

    feat: Update typescript configuration for building

--- packages/core/.babelrc.js
+++ packages/core/.babelrc.js
@@ -1,1 +1,4 @@
-module.exports = require('../../babel')({ styledComponents: true, react: true, inlineSvg: true });
+module.exports = {
+  ...require('../../babel')({ styledComponents: true, react: true, inlineSvg: true }),
+  ignore: ['src/**/*.d.ts'],
+};
