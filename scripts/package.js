d724b6b9786d4770e8494848e4b54a703d1d67f7
drizzer14 <drizzer14@gmail.com>
2020-09-08 14:33:18 +0300

    chore: Update package.json generation

--- scripts/package.js
+++ scripts/package.js
@@ -2,10 +2,21 @@ const fromTemplate = require('./from-template');
 
 const fileName = 'package.json';
 
+const meta = {
+  author: 'uStudio Company <toberead@ustudio.company> (http://ustudio.company)',
+  repository: 'github:uStudioCompany/ustudio-ui',
+  homepage: 'https://github.com/uStudioCompany/ustudio-ui/blob/master/README.md',
+  license: 'MIT',
+};
+
 fromTemplate(
   fileName,
-  ({ scripts: _s, ...template }) => {
+  ({ scripts: _s, name, version, keywords = [], ...template }) => {
     return {
+      name,
+      version,
+      ...meta,
+      keywords: ['react', 'typescript', ...keywords],
       ...template,
     };
   },
