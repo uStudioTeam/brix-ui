8478675858fb28b0af7fc04602a619ce4eef58c5
drizzer14 <drizzer14@gmail.com>
2020-09-09 11:56:43 +0300

    test(Disclosure): remove `height` tests

--- packages/core/tests/disclosure.spec.tsx
+++ packages/core/tests/disclosure.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, fireEvent, waitFor, RenderResult } from '@testing-library/react';
 import 'jest-styled-components';
 
 import ThemeProvider from '@ustudio-ui/theme';
@@ -9,12 +9,19 @@ import Disclosure, { DisclosureProps } from '../src/disclosure';
 
 const disclosureId = 'disclosure';
 
-const renderWithProps = (props: Partial<DisclosureProps> = {}) => {
-  return render(
+const renderWithProps = (props: Partial<DisclosureProps> = {}): RenderResult & { disclosure: HTMLElement } => {
+  const renderResult = render(
     <ThemeProvider>
       <Disclosure icon={() => <div />} {...props} data-testid={disclosureId} />
     </ThemeProvider>
   );
+
+  const disclosure = renderResult.getByTestId(disclosureId);
+
+  return {
+    ...renderResult,
+    disclosure,
+  };
 };
 
 const click = (element: HTMLElement): void => {
@@ -24,108 +31,71 @@ const click = (element: HTMLElement): void => {
 describe('<Disclosure />', () => {
   matchMedia();
 
-  describe('open/close state', () => {
-    describe('when uncontrolled', () => {
-      it('should open/close without external state control', () => {
-        const { getByTestId } = renderWithProps();
-
-        click(getByTestId(disclosureId));
-
-        waitFor(() => {
-          expect(getByTestId(disclosureId).querySelector('button + div')).toHaveStyleRule('height', 'auto');
-        });
-
-        click(getByTestId(disclosureId));
-
-        waitFor(() => {
-          expect(getByTestId(disclosureId).querySelector('button + div')).toHaveStyleRule('height', 0);
-        });
-      });
-    });
-
-    describe('when controlled', () => {
-      it('should open/close when `isOpen` prop changes', () => {
-        const { getByTestId, rerender } = render(
-          <ThemeProvider>
-            <Disclosure data-testid={disclosureId} icon={() => <div />} />
-          </ThemeProvider>
-        );
-
-        waitFor(() => {
-          expect(getByTestId(disclosureId).querySelector('button + div')).toHaveStyleRule('height', 'auto');
-        });
-
-        rerender(
-          <ThemeProvider>
-            <Disclosure isOpen data-testid={disclosureId} icon={() => <div />} />
-          </ThemeProvider>
-        );
-
-        waitFor(() => {
-          expect(getByTestId(disclosureId).querySelector('button + div')).toHaveStyleRule('height', 0);
-        });
-      });
-    });
-  });
-
-  /* These fail due to empty JSDOM rendering
   describe('state change callbacks', () => {
     describe('onChange', () => {
-      it('should call `onChange` on each open state change', () => {
+      it('should call `onChange` on each open state change', async () => {
         const onChange = jest.fn();
 
-        const { getByTestId } = renderWithProps({
+        const { disclosure } = renderWithProps({
           onChange,
         });
 
-        click(getByTestId(disclosureId));
+        click(disclosure);
 
-        click(getByTestId(disclosureId));
+        await waitFor(() => {
+          expect(onChange).toHaveBeenCalledTimes(1);
+        });
 
-        waitFor(() => {
+        click(disclosure);
+
+        await waitFor(() => {
           expect(onChange).toHaveBeenCalledTimes(2);
         });
       });
     });
 
     describe('onOpen', () => {
-      it('should call `onOpen` when next open state would be `true`', () => {
+      it('should call `onOpen` when next open state would be `true`', async () => {
         const onOpen = jest.fn();
 
-        const { getByTestId } = renderWithProps({
+        const { disclosure } = renderWithProps({
           onOpen,
         });
 
-        click(getByTestId(disclosureId));
+        click(disclosure);
 
-        waitFor(() => {
+        await waitFor(() => {
           expect(onOpen).toHaveBeenCalledTimes(1);
         });
 
-        click(getByTestId(disclosureId));
+        click(disclosure);
 
-        waitFor(() => {
+        await waitFor(() => {
           expect(onOpen).toHaveBeenCalledTimes(1);
         });
       });
     });
 
     describe('onClose', () => {
-      it('should call `onClose` when next open state would be `false`', () => {
+      it('should call `onClose` when next open state would be `false`', async () => {
         const onClose = jest.fn();
 
-        const { getByTestId } = renderWithProps({
+        const { disclosure } = renderWithProps({
           onClose,
         });
 
-        click(getByTestId(disclosureId));
+        click(disclosure);
 
-        click(getByTestId(disclosureId));
+        await waitFor(() => {
+          expect(onClose).not.toHaveBeenCalled();
+        });
 
-        waitFor(() => {
+        click(disclosure);
+
+        await waitFor(() => {
           expect(onClose).toHaveBeenCalledTimes(1);
         });
       });
     });
-  }); */
+  });
 });
