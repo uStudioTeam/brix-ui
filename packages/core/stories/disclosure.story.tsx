dc7a5670a74931b8306e3d38a66e2b480ba28268
drizzer14 <drizzer14@gmail.com>
2020-09-10 12:46:07 +0300

    feat(Disclosure): Update styles and add `Stylable` story

--- packages/core/stories/disclosure.story.tsx
+++ packages/core/stories/disclosure.story.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Story } from '@storybook/react';
+import styled, { css } from 'styled-components';
 
 import Disclosure, { DisclosureProps } from '@ustudio-ui/core/disclosure';
+
+import Flex from '@ustudio-ui/core/flex';
+import Chevron from '../assets/icons/chevron.inline.svg';
 
 export default {
   title: 'Widgets/Disclosure',
@@ -23,4 +27,50 @@ export default {
 
 export const Basic: Story<DisclosureProps> = (args) => {
   return <Disclosure {...args}>Disclosed!</Disclosure>;
+};
+
+const Container = styled(Flex).attrs(() => ({
+  direction: 'column',
+}))`
+  border-color: var(--c-accent-weak);
+`;
+
+const Icon = styled(Chevron)``;
+
+const Summary = styled.button<{
+  isOpen: boolean;
+}>(
+  ({ isOpen }) => css`
+    &,
+    ${Icon} {
+      color: var(--c-text-base-weak);
+    }
+
+    ${isOpen
+      ? css`
+          background-color: var(--c-accent-strong-down);
+        `
+      : css`
+          background-color: var(--c-accent-strong);
+        `};
+
+    &:focus {
+      background-color: var(--c-accent-strong-down);
+    }
+  `
+);
+
+export const Stylable: Story<DisclosureProps> = (args) => {
+  return (
+    <Disclosure
+      {...args}
+      styled={{
+        Container,
+        Summary,
+        Icon,
+      }}
+    >
+      Styled & Disclosed!
+    </Disclosure>
+  );
 };
