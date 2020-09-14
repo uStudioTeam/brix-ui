ddea76f572b3518b4d8c41be74cc5084ab30d7e4
drizzer14 <drizzer14@gmail.com>
2020-09-14 14:29:11 +0300

    feat(Spinner): Add style optimizations

--- packages/core/src/spinner/spinner.component.tsx
+++ packages/core/src/spinner/spinner.component.tsx
@@ -1,4 +1,5 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
+import { Keyframes, keyframes } from 'styled-components';
 
 import { intrinsicComponent } from '@ustudio-ui/utils/functions';
 
@@ -25,27 +26,63 @@ const Spinner = intrinsicComponent<SpinnerProps, HTMLDivElement>(function Spinne
   { blades = 9, bladeSize, speed = 150, color, opacity, swirl, spread = 1, className, styles },
   ref
 ) {
+  const [opacityFrom, opacityTo] = [opacity?.[0] || 0.25, opacity?.[1] || 1];
   const animationDuration = useMemo(() => blades * speed, [blades, speed]);
   const timingShift = useMemo(() => 100 / blades, [blades]);
   const { width, height } = useMemo(() => parseBladeSize(bladeSize), [JSON.stringify(bladeSize)]);
 
-  const bladeProps = {
-    blades,
-    $width: width,
-    $height: height,
-    $color: color,
-    $opacity: opacity,
-    swirl,
-    spread,
-    animationDuration,
-    timingShift,
-  };
+  const animations = useCallback(
+    (index: number): Keyframes => {
+      // Maximum point, %
+      const extremumMax = (index / blades) * 100;
+      // Minimum points, %
+      const extremumMinStart = extremumMax - timingShift;
+      const extremumMinEnd = extremumMax + timingShift;
+      const extremumMinStartChomped = extremumMinStart >= 0 ? extremumMinStart : 100 - Math.abs(extremumMinStart);
+      const extremumMinEndChomped = extremumMinEnd <= 100 ? extremumMinEnd : Math.abs(100 - extremumMinEnd);
+
+      return keyframes`
+        ${extremumMinStartChomped}%,
+        ${extremumMinEndChomped}% {
+          opacity: ${opacityFrom};
+        }
+    
+        ${extremumMax}% {
+          opacity: ${opacityTo};
+      }`;
+    },
+    [blades, opacityFrom, opacityTo, timingShift]
+  );
+
+  const transform = useCallback(
+    (index: number): string => {
+      return `
+        rotate(${(index / blades) * 360 - 90}deg)
+        translate(calc(-50% - ${(blades + 5) * spread}px), calc(${swirl ? blades * 10 : -50}%))
+      `;
+    },
+    [blades, spread, swirl]
+  );
 
   return (
     <Styled.Spinner as={styles?.Spinner} ref={ref} className={className}>
       {[...new Array(blades).keys()].map((_, index) => {
-        // eslint-disable-next-line react/no-array-index-key
-        return <Styled.Blade as={styles?.Blade} key={index} index={index} {...bladeProps} />;
+        return (
+          <Styled.Blade
+            as={styles?.Blade}
+            // eslint-disable-next-line react/no-array-index-key
+            key={index}
+            animation={animations(index)}
+            animationDuration={animationDuration}
+            $width={width}
+            $height={height}
+            $color={color}
+            style={{
+              transform: transform(index),
+              opacity: index ? opacityFrom : opacityTo,
+            }}
+          />
+        );
       })}
     </Styled.Spinner>
   );
--- packages/core/src/spinner/spinner.styles.ts
+++ packages/core/src/spinner/spinner.styles.ts
@@ -1,4 +1,4 @@
-import styled, { css, Keyframes, keyframes } from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { applyDisplayNames } from '@ustudio-ui/utils/functions';
 
@@ -11,90 +11,33 @@ export const Spinner = styled.div`
   height: 100%;
 `;
 
-const animation = ({
-  index,
-  blades,
-  timingShift,
-  opacity: [opacityFrom, opacityTo],
-}: {
-  index: number;
-  blades: number;
-  timingShift: number;
-  opacity: [number, number];
-}): Keyframes => {
-  // Maximum point, %
-  const extremumMax = (index / blades) * 100;
-  // Minimum points, %
-  const extremumMinStart = extremumMax - timingShift;
-  const extremumMinEnd = extremumMax + timingShift;
-  const extremumMinStartChomped = extremumMinStart >= 0 ? extremumMinStart : 100 - Math.abs(extremumMinStart);
-  const extremumMinEndChomped = extremumMinEnd <= 100 ? extremumMinEnd : Math.abs(100 - extremumMinEnd);
+export const Blade = styled.div<{
+  $width: string;
+  $height: string;
+  $color: SpinnerProps['color'];
 
-  return keyframes`
-      ${extremumMinStartChomped}%,
-      ${extremumMinEndChomped}% {
-        opacity: ${opacityFrom};
-      }
-  
-      ${extremumMax}% {
-        opacity: ${opacityTo};
-    }`;
-};
+  animation: string;
+  animationDuration: number;
+}>(({ theme, $width, $height, $color = 'faint-strong', animation, animationDuration }) => {
+  const color = theme.colorHelper.parseColor($color);
 
-export const Blade = styled.div<
-  Required<Omit<SpinnerProps, 'color' | 'opacity' | 'speed' | 'bladeSize'>> & {
-    $width: string;
-    $height: string;
-    $color: SpinnerProps['color'];
-    $opacity: SpinnerProps['opacity'];
+  return css`
+    width: ${$width};
+    height: ${$height};
 
-    animationDuration: number;
-    timingShift: number;
-    index: number;
-  }
->(
-  ({
-    theme,
-    blades,
-    swirl,
-    spread,
-    $width,
-    $height,
-    $color = 'faint-strong',
-    $opacity: opacity = [],
-    animationDuration,
-    timingShift,
-    index,
-  }) => {
-    const [opacityFrom = 0.25, opacityTo = 1] = opacity;
-    const color = theme.colorHelper.parseColor($color);
+    position: absolute;
+    left: 50%;
+    top: 50%;
 
-    return css`
-      width: ${$width};
-      height: ${$height};
+    border-radius: ${Number.parseFloat($height) / 2}px;
+    background-color: ${color};
 
-      position: absolute;
-      left: 50%;
-      top: 50%;
+    animation: ${animation} ${animationDuration}ms infinite;
 
-      opacity: ${index ? opacityFrom : opacityTo};
-      border-radius: ${Number.parseFloat($height) / 2}px;
-      background-color: ${color};
-
-      animation: ${animation({
-          index,
-          blades,
-          timingShift,
-          opacity: [opacityFrom, opacityTo],
-        })}
-        ${animationDuration}ms infinite;
-
-      transform: rotate(${(index / blades) * 360 - 90}deg)
-        translate(calc(-50% - ${(blades + 5) * spread}px), calc(${swirl ? blades * 10 : -50}%));
-      transform-origin: left top;
-    `;
-  }
-);
+    transform-origin: left top;
+    transition: all 200ms;
+  `;
+});
 
 const Styled = applyDisplayNames({ Spinner, Blade });
 
