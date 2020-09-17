011c5be0aac7176b4a771fc3d8a880349ca712d7
drizzer14 <drizzer14@gmail.com>
2020-09-17 10:29:20 +0300

    feat(core): Add `Affix` internal component

---
+++ packages/core/src/_internal/affix/affix.props.d.ts
@@ -0,0 +1,5 @@
+import type { HTMLAttributes } from 'react';
+
+import type { IntrinsicComponent, StylableComponent } from '@ustudio-ui/types/component';
+
+export interface AffixProps extends IntrinsicComponent<HTMLAttributes<HTMLElement>>, StylableComponent {}
