c42318b68a38aedc6e564098c7118be068afa3e2
drizzer14 <drizzer14@gmail.com>
2020-09-21 12:51:09 +0300

    fix: Fix usage of `extract` prop-types function

--- packages/prop-types/src/utils/extract.ts
+++ packages/prop-types/src/utils/extract.ts
@@ -1,11 +1,7 @@
-import { filterObject } from '@ustudio-ui/utils/functions';
 import type { FC, WeakValidationMap } from 'react';
 
 import { merge } from './merge';
 
-export const extract = <P = {}, K extends keyof P = keyof P>(
-  components: FC<P>[],
-  filterKeys?: (key: string) => boolean
-): WeakValidationMap<Omit<P, K>> => {
-  return merge(...components.map(({ propTypes = {} }) => filterObject(propTypes, filterKeys || (() => true))));
+export const extract = <P extends Record<string, any> = {}>(components: FC<P>[]): WeakValidationMap<P> => {
+  return merge(...components.map(({ propTypes = {} }) => propTypes));
 };
