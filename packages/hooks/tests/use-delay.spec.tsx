e8ef17b962a2403246ba481aafc92b0842d717a9
drizzer14 <drizzer14@gmail.com>
2020-09-30 16:29:32 +0300

    test(hooks): Add `useDelay` tests

---
+++ packages/hooks/tests/use-delay.spec.tsx
@@ -0,0 +1,35 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+
+import useDelay from '../src/use-delay';
+
+const render = (delay: number | undefined) => {
+  return renderHook(() => useDelay(delay));
+};
+
+jest.useFakeTimers();
+
+describe('useDelay', () => {
+  describe('when delay is not specified', () => {
+    it('should render immediately', async () => {
+      const { result, waitFor } = render(undefined);
+
+      jest.advanceTimersByTime(0);
+
+      await waitFor(() => expect(result.current).toBe(true));
+    });
+  });
+
+  describe('when delay is specified', () => {
+    it('should render only after the specified time will have passed', async () => {
+      const { result, waitFor } = render(54);
+
+      await waitFor(() => expect(result.current).toBe(false));
+
+      act(() => {
+        jest.advanceTimersByTime(54);
+      });
+
+      await waitFor(() => expect(result.current).toBe(true));
+    });
+  });
+});
