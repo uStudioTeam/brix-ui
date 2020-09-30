b9a1fc82f3d80e19dfa8ca781a210f3aa9146636
drizzer14 <drizzer14@gmail.com>
2020-09-30 16:28:34 +0300

    fix: Add `hooks` as root for `jest`

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
     .flatMap(([prevPath]) => {
       const path = prevPath.replace(/\/\*/, '');
 
-      if (['core', 'grid'].includes(path.split('/')[1])) {
+      if (['core', 'grid', 'hooks'].includes(path.split('/')[1])) {
         return [path, path.replace(/src/, 'tests')];
       }
 
