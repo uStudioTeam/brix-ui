976cf7e833086e6764a9705e9f15f6d0fe19f92a
drizzer14 <drizzer14@gmail.com>
2020-09-30 11:39:08 +0300

    feat(Modal): Update `Modal` context and hook

--- packages/contexts/src/modal/modal.context.tsx
+++ packages/contexts/src/modal/modal.context.tsx
@@ -2,21 +2,21 @@ import React, { createContext, FC, useContext } from 'react';
 
 import useDisclose from '@brix-ui/hooks/use-disclose';
 import useUnmountOnExit from '@brix-ui/hooks/use-unmount-on-exit';
-import { filterObject } from '@brix-ui/utils/functions';
+import { applyPolymorphicFunctionProp } from '@brix-ui/utils/functions';
 import { disclosable, unmountable } from '@brix-ui/prop-types/common';
 
-import type { ModalProps } from './modal.props';
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface ModalValue extends Pick<ModalProps, 'unmountOnExit'> {
-  shouldBeOpen: boolean;
-  shouldMount: boolean;
-}
+import type { ModalProps, ModalValue } from './modal.props';
 
 const ModalContext = createContext<ModalValue | undefined>(undefined);
 
-const Modal: FC<ModalProps> = ({ children, isOpen, unmountOnExit, onOpen, onChange, onClose }) => {
-  const [internalIsOpen] = useDisclose({
+export const useModal = ({
+  isOpen,
+  onOpen,
+  onChange,
+  onClose,
+  unmountOnExit,
+}: Omit<ModalProps, 'children'>): ModalValue => {
+  const [internalIsOpen, toggle] = useDisclose({
     isOpen,
     onOpen,
     onChange,
@@ -25,23 +25,20 @@ const Modal: FC<ModalProps> = ({ children, isOpen, unmountOnExit, onOpen, onChan
 
   const [shouldBeOpen, shouldMount] = useUnmountOnExit(internalIsOpen, unmountOnExit);
 
-  return shouldMount ? (
-    <ModalContext.Provider value={{ unmountOnExit, shouldBeOpen, shouldMount }}>{children}</ModalContext.Provider>
+  return useContext(ModalContext) || { shouldBeOpen, shouldMount, toggle };
+};
+
+const Modal: FC<ModalProps> = ({ children, isOpen, unmountOnExit, onOpen, onChange, onClose }) => {
+  const value = useModal({ isOpen, onOpen, onChange, onClose, unmountOnExit });
+
+  return value.shouldMount ? (
+    <ModalContext.Provider value={value}>{applyPolymorphicFunctionProp(children, value)}</ModalContext.Provider>
   ) : null;
 };
 
 Modal.propTypes = {
   ...disclosable,
   ...unmountable,
-};
-
-export const useModal = (props = {} as Partial<ModalValue>): ModalValue => {
-  const context = useContext(ModalContext) || {};
-
-  return {
-    ...filterObject(props, (key) => props[key] !== undefined),
-    ...context,
-  } as ModalValue;
 };
 
 export default Modal;
--- packages/core/src/drawer/drawer.component.tsx
+++ packages/core/src/drawer/drawer.component.tsx
@@ -4,8 +4,6 @@ import PT from 'prop-types';
 import { Position } from '@brix-ui/types/css';
 import { disclosable, stylableComponent, unmountable } from '@brix-ui/prop-types/common';
 import { intrinsicComponent, objectValues } from '@brix-ui/utils/functions';
-import useDisclose from '@brix-ui/hooks/use-disclose';
-import useUnmountOnExit from '@brix-ui/hooks/use-unmount-on-exit';
 import { useModal } from '@brix-ui/contexts/modal';
 
 import Portal from '../portal';
@@ -17,19 +15,7 @@ const Drawer = intrinsicComponent<DrawerProps, HTMLDivElement>(function Drawer(
   { children, position, unmountOnExit, isOpen, onOpen, onChange, onClose, ...props },
   ref
 ) {
-  const [internalIsOpen] = useDisclose({
-    isOpen,
-    onOpen,
-    onChange,
-    onClose,
-  });
-
-  const useUnmountOnExitResult = useUnmountOnExit(internalIsOpen, unmountOnExit);
-
-  const { shouldMount, shouldBeOpen } = useModal({
-    ...useUnmountOnExitResult,
-    unmountOnExit,
-  });
+  const { shouldBeOpen, shouldMount } = useModal({ isOpen, onOpen, onChange, onClose, unmountOnExit });
 
   return shouldMount ? (
     <Portal>
