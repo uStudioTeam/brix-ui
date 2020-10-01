6d29d522288e65234102ea06757b43cfd3d35e3d
drizzer14 <drizzer14@gmail.com>
2020-10-01 16:39:00 +0300

    fix(useDisclose): Fix `useDisclose` and `Disclosable` components behaviour, add tests

--- packages/hooks/src/use-disclose.ts
+++ packages/hooks/src/use-disclose.ts
@@ -1,7 +1,10 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useCallback, useMemo } from 'react';
 
 import { tryCall } from '@brix-ui/utils/functions';
 import type { Disclosable } from '@brix-ui/types/component';
+
+import useUpdateEffect from './use-update-effect';
+import useUpdatedState from './use-updated-state';
 
 export default function useDisclose({
   isOpen,
@@ -9,31 +12,22 @@ export default function useDisclose({
   onChange,
   onClose,
 }: Disclosable): [internalIsOpen: boolean, toggle: (isOpen?: boolean) => void] {
-  const [internalIsOpen, setInternalOpen] = useState(isOpen ?? false);
-  const hasChangedRef = useRef(false);
+  const [internalIsOpen, setInternalOpen] = useUpdatedState(isOpen ?? false);
 
   const toggle = useCallback(
     (openControl?: boolean) => {
-      hasChangedRef.current = true;
-
       setInternalOpen(openControl === undefined ? !internalIsOpen : openControl);
     },
     [internalIsOpen, setInternalOpen]
   );
 
-  useEffect(() => {
-    if (isOpen !== undefined) {
-      toggle(isOpen);
-    }
-  }, [isOpen, toggle]);
-
-  useEffect(() => {
-    if (hasChangedRef.current) {
+  useUpdateEffect(() => {
+    if ((isOpen !== undefined && isOpen === internalIsOpen) || isOpen === undefined) {
       tryCall(internalIsOpen ? onOpen : onClose);
 
       tryCall(onChange, internalIsOpen);
     }
-  }, [internalIsOpen, onOpen, onChange, onClose]);
+  }, [internalIsOpen, isOpen, onOpen, onChange, onClose]);
 
   return useMemo(() => [internalIsOpen, toggle], [internalIsOpen, toggle]);
 }
