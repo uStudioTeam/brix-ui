2383fce43358f7a570caf2cc70ccf343eebca119
drizzer14 <drizzer14@gmail.com>
2020-10-07 11:50:58 +0300

    fix(prop-types): Fix `refProp` implementation

--- packages/prop-types/src/common/ref-prop.ts
+++ packages/prop-types/src/common/ref-prop.ts
@@ -2,5 +2,5 @@ import { Ref } from 'react';
 import PT, { Requireable } from 'prop-types';
 
 export const refProp = <E extends HTMLElement>(): Requireable<Ref<E>> => {
-  return PT.func;
+  return PT.oneOfType([PT.func, PT.object]) as Requireable<Ref<E>>;
 };
