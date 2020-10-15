5960b57e4b2bf376dd2d0c9881957420563655f7
drizzer14 <drizzer14@gmail.com>
2020-10-15 11:59:14 +0300

    feat(theme): Add `transition` mixin

--- packages/theme/src/mixin/index.ts
+++ packages/theme/src/mixin/index.ts
@@ -2,4 +2,5 @@ export * from './size';
 export * from './font';
 export * from './shadow';
 export * from './hidden';
+export * from './transition';
 export * from './compensate-line-height';
---
+++ packages/theme/src/mixin/transition.ts
@@ -0,0 +1,6 @@
+export const transition = (duration: string, ...properties: string[]): string => {
+  return `${properties
+    .reduce((style, property) => `${style}, ${property} ${duration}`, ``)
+    .trim()
+    .slice(1)}`;
+};
