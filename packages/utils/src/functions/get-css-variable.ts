71faa36caef95bd839641b8b5bcb3252a629d7d8
drizzer14 <drizzer14@gmail.com>
2020-10-20 17:37:34 +0300

    feat: Remove some unused utils and types

--- packages/utils/src/functions/get-css-variable.ts
+++
@@ -1,7 +0,0 @@
-import { Variable } from '@brix-ui/types/css';
-
-import type { Values } from '../types';
-
-export function getCssVariable(prefix: Values<typeof Variable>, name: string): string {
-  return `var(--${prefix}-${name})`;
-}
