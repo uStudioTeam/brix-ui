7e57513e3334603d8995894dc6b6765ac24a7013
drizzer14 <drizzer14@gmail.com>
2020-10-23 10:30:45 +0300

    fix(prop-types): Fix `record` type declaration

--- packages/prop-types/src/utils/record.ts
+++ packages/prop-types/src/utils/record.ts
@@ -1,6 +1,4 @@
-import type { Requireable } from 'prop-types';
-
-export const record = <T extends string, P extends Requireable<unknown>>(keys: T[], valueType: P): Record<T, P> => {
+export const record = <T extends string, P>(keys: T[], valueType: P): Record<T, P> => {
   return keys.reduce((toObject, key) => {
     return Object.assign(toObject, {
       [key]: valueType,
