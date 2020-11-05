ffb3afc2f4d4eac3544cacf3a46df0103598aaac
drizzer14 <drizzer14@gmail.com>
2020-11-05 12:05:56 +0200

    docs: Add default fonts

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -5,7 +5,113 @@ import { createGlobalStyle } from 'styled-components';
 import ThemeProvider from '@brix-ui/theme';
 import { useTheme } from '@brix-ui/theme/hooks';
 import { ThemeMode } from '@brix-ui/theme/entity';
-import Fonts from '@brix-ui/fonts';
+import { FontWeight } from '@brix-ui/types/typography';
+
+import SSPBlack from './public/assets/fonts/SourceSansPro/SourceSansPro-Black.ttf';
+import SSPBold from './public/assets/fonts/SourceSansPro/SourceSansPro-Bold.ttf';
+import SSPSemiBold from './public/assets/fonts/SourceSansPro/SourceSansPro-SemiBold.ttf';
+import SSPRegular from './public/assets/fonts/SourceSansPro/SourceSansPro-Regular.ttf';
+import SSPLight from './public/assets/fonts/SourceSansPro/SourceSansPro-Light.ttf';
+
+import MBlack from './public/assets/fonts/Merriweather/Merriweather-Black.ttf';
+import MRegular from './public/assets/fonts/Merriweather/Merriweather-Regular.ttf';
+import MLight from './public/assets/fonts/Merriweather/Merriweather-Light.ttf';
+
+import IExtraBold from './public/assets/fonts/Inconsolata/Inconsolata-ExtraBold.ttf';
+import IRegular from './public/assets/fonts/Inconsolata/Inconsolata-Regular.ttf';
+import ILight from './public/assets/fonts/Inconsolata/Inconsolata-Light.ttf';
+
+const formatSrc = (url) => {
+  return `url(${url}) format('truetype')`;
+};
+
+const body = {
+  black: formatSrc(SSPBlack),
+  bold: formatSrc(SSPBold),
+  semiBold: formatSrc(SSPSemiBold),
+  regular: formatSrc(SSPRegular),
+  light: formatSrc(SSPLight),
+};
+
+const article = {
+  black: formatSrc(MBlack),
+  regular: formatSrc(MRegular),
+  light: formatSrc(MLight),
+};
+
+const code = {
+  extraBold: formatSrc(IExtraBold),
+  regular: formatSrc(IRegular),
+  light: formatSrc(ILight),
+};
+
+const Fonts = createGlobalStyle`
+  @font-face {
+    font-family: 'Source Sans Pro';
+    font-weight: ${FontWeight.Black};
+    src: ${body.black};
+  }
+  
+  @font-face {
+    font-family: 'Source Sans Pro';
+    font-weight: ${FontWeight.Bold};
+    src: ${body.bold};
+  }
+  
+  @font-face {
+    font-family: 'Source Sans Pro';
+    font-weight: ${FontWeight.SemiBold};
+    src: ${body.semiBold};
+  }
+  
+  @font-face {
+    font-family: 'Source Sans Pro';
+    font-weight: ${FontWeight.Regular};
+    src: ${body.regular};
+  }
+  
+  @font-face {
+    font-family: 'Source Sans Pro';
+    font-weight: ${FontWeight.Light};
+    src: ${body.light};
+  }
+  
+  @font-face {
+    font-family: Merriweather;
+    font-weight: ${FontWeight.Black};
+    src: ${article.black};
+  }
+  
+  @font-face {
+    font-family: Merriweather;
+    font-weight: ${FontWeight.Regular};
+    src: ${article.regular};
+  }
+  
+  @font-face {
+    font-family: Merriweather;
+    font-weight: ${FontWeight.Light};
+    src: ${article.light};
+  }
+  
+  @font-face {
+    font-family: Inconsolata;
+    font-weight: ${FontWeight.ExtraBold};
+    src: ${code.extraBold};
+  }
+  
+  @font-face {
+    font-family: Inconsolata;
+    font-weight: ${FontWeight.Regular};
+    src: ${code.regular};
+  }
+  
+  @font-face {
+    font-family: Inconsolata;
+    font-weight: ${FontWeight.Light};
+    src: ${code.light};
+  }
+`;
 
 export const globalTypes = {
   theme: {
