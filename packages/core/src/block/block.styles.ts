8153739d37110bac7f0f286fe2f0239b91603c5c
drizzer14 <drizzer14@gmail.com>
2020-11-09 17:04:08 +0200

    feat(utils): Move `parseIndent` function from `Block` to `utils`

--- packages/core/src/block/block.styles.ts
+++ packages/core/src/block/block.styles.ts
@@ -1,47 +1,8 @@
 import styled, { css, FlattenSimpleInterpolation } from 'styled-components';
 
-import { Position } from '@brix-ui/types/css';
-import { applyDisplayNames, isUndefined, objectValues } from '@brix-ui/utils/functions';
-import { Axis, Indent, PositionIndent } from '@brix-ui/types/component';
+import { applyDisplayNames, parseIndent } from '@brix-ui/utils/functions';
 
 import type { BlockProps } from './block.props';
-
-const reduceToStyles = <P extends string, R extends Partial<Record<P, string>>>(
-  object: R | undefined,
-  parameters: readonly P[]
-): string => {
-  return parameters.reduce((style, position) => {
-    return `${style} ${object?.[position] ?? 0}`;
-  }, '');
-};
-
-const parseIndent = (indent?: Indent): string => {
-  if (isUndefined(indent) || Object.is(indent, {})) {
-    return '';
-  }
-
-  if (typeof indent === 'string') {
-    return indent;
-  }
-
-  if ('vertical' in indent) {
-    if ('left' in indent || 'right' in indent) {
-      return reduceToStyles(indent, ['vertical', 'right', 'vertical', 'left'] as const);
-    }
-
-    return reduceToStyles(indent, [Axis.Vertical, Axis.Horizontal]);
-  }
-
-  if ('horizontal' in indent) {
-    if ('top' in indent || 'bottom' in indent) {
-      return reduceToStyles(indent, ['top', 'horizontal', 'bottom'] as const);
-    }
-
-    return reduceToStyles(indent, [Axis.Vertical, Axis.Horizontal]);
-  }
-
-  return reduceToStyles(indent as PositionIndent, objectValues(Position));
-};
 
 const parseGap = (gap: BlockProps['gap']): FlattenSimpleInterpolation | undefined => {
   if (!gap) {
