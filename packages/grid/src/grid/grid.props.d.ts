9e099f68838fb6b86465cd2aea6f2d2eaabfe73e
drizzer14 <drizzer14@gmail.com>
2020-11-10 12:29:16 +0200

    feat(Grid): update `Grid` component, now extending from `Flex`

--- packages/grid/src/grid/grid.props.d.ts
+++ packages/grid/src/grid/grid.props.d.ts
@@ -1,27 +1,13 @@
-import type { HTMLAttributes, PropsWithChildren } from 'react';
+import type { FlexProps } from '@brix-ui/core/flex';
+import type { BreakpointsProps, Gapable, PolymorphicBreakpointProp } from '@brix-ui/types/component';
 
-import type { BlockProps } from '@brix-ui/core/block';
-import type {
-  IntrinsicComponent,
-  StylableComponent,
-  BreakpointsProps,
-  PolymorphicBreakpointProp,
-} from '@brix-ui/types/component';
-import type { Values } from '@brix-ui/utils/types';
-import { Direction } from '@brix-ui/types/css';
-
-export interface GridBreakpointProps extends Pick<BlockProps, 'gap'> {
-  direction?: Values<typeof Direction>;
-  /**
-   * Did not use PolymorphicBreakpointProp here
-   * as it may mislead to having a different argument available
-   */
+// Did not use PolymorphicBreakpointProp on the `template`
+// as it may mislead to having a different argument available
+export interface GridBreakpointProps extends Pick<FlexProps, 'direction' | Extract<keyof Gapable, 'gap'>> {
   template?: ((fractionsCount: number) => string) | string;
   maxWidth?: PolymorphicBreakpointProp;
 }
 
 export interface GridProps
   extends BreakpointsProps<GridBreakpointProps>,
-    PropsWithChildren<IntrinsicComponent<HTMLAttributes<HTMLDivElement>>>,
-    StylableComponent,
-    Omit<BlockProps, 'isInline' | 'gap'> {}
+    Omit<FlexProps, 'isReversed' | 'hasWrap' | Exclude<keyof Gapable, 'gap'>> {}
--- packages/grid/src/grid/grid.styles.ts
+++ packages/grid/src/grid/grid.styles.ts
@@ -1,47 +1,33 @@
-import styled, { css, FlattenSimpleInterpolation } from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { applyDisplayNames } from '@brix-ui/utils/functions';
-import Block from '@brix-ui/core/block';
 import { Direction } from '@brix-ui/types/css';
+import Flex from '@brix-ui/core/flex';
 
 import type { GridProps } from './grid.props';
 
-const parseGap = (gap: GridProps['gap']): FlattenSimpleInterpolation | undefined => {
-  if (!gap) {
-    return;
-  }
-
-  if (typeof gap === 'string') {
-    return css`
-      grid-gap: ${gap};
-    `;
-  }
-
-  return css`
-    grid-row-gap: ${gap.vertical};
-    grid-column-gap: ${gap.horizontal};
-  `;
-};
-
-const Grid = styled(Block)<{
-  $direction: GridProps['direction'];
+const Grid = styled(Flex)<{
   $gap: GridProps['gap'];
   $maxWidth: Extract<GridProps['maxWidth'], string> | undefined;
   template: Extract<GridProps['template'], string> | undefined;
   areas: string;
   fractionsCount: number;
-}>(({ $direction: direction, $gap: gap, $maxWidth: maxWidth, areas, template, fractionsCount }) => {
+}>(({ direction, $gap: gap, $maxWidth: maxWidth, areas, template, fractionsCount }) => {
   const templateDirection = direction === Direction.Column ? Direction.Row : Direction.Column;
 
   return css`
     display: grid;
 
-    ${parseGap(gap)};
+    grid-gap: ${gap};
 
     grid-template-areas: ${areas};
     ${`grid-template-${templateDirection}s`}: ${template || `repeat(${fractionsCount}, 1fr)`};
 
     max-width: ${maxWidth};
+
+    &[data-inline] {
+      display: inline-grid;
+    }
   `;
 });
 
