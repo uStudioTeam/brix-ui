49cccc6136cd830f721dbe5a3af016d9db292103
drizzer14 <drizzer14@gmail.com>
2020-11-10 12:29:39 +0200

    feat(Cell): update `Cell` component, now extending from `Flex`

--- packages/grid/src/area-builder/entity/cell.d.ts
+++ packages/grid/src/area-builder/entity/cell.d.ts
@@ -1,6 +1,7 @@
 import type { CellProps } from '../../cell';
 
-export interface Cell extends Pick<CellProps, 'offset'> {
+export interface Cell {
   id: string;
   size?: number;
+  offset?: Exclude<CellProps['offset'], number>;
 }
