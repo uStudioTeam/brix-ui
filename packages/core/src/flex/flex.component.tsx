5de25b092b46173d7e644ece9234a1306d5d27e1
drizzer14 <drizzer14@gmail.com>
2020-11-10 12:25:01 +0200

    feat(core): update `Block` and `Flex` implementations with new `Gapable` type & more

--- packages/core/src/flex/flex.component.tsx
+++ packages/core/src/flex/flex.component.tsx
@@ -3,8 +3,8 @@ import PT from 'prop-types';
 
 import { classNames, intrinsicComponent, objectValues, orUndefined } from '@brix-ui/utils/functions';
 import type { FlexElement } from '@brix-ui/types/html';
-import { Direction as DirectionType, FlexContainer } from '@brix-ui/types/css';
-import { alignable, stylableComponent, taggable } from '@brix-ui/prop-types/common';
+import { Direction as DirectionType } from '@brix-ui/types/css';
+import { alignable, stylableComponent } from '@brix-ui/prop-types/common';
 import { extract } from '@brix-ui/prop-types/utils';
 import Direction from '@brix-ui/contexts/direction';
 
@@ -14,11 +14,11 @@ import type { FlexProps } from './flex.props';
 import Styled from './flex.styles';
 
 const Flex = intrinsicComponent<FlexProps, FlexElement>(function Flex(
-  { children, className, as, direction = DirectionType.Row, align, hasWrap, isInline, ...props },
+  { children, className, as, direction, align, hasWrap, isInline, ...props },
   ref
 ) {
   return (
-    <Direction value={direction}>
+    <Direction value={direction || DirectionType.Row}>
       <Styled.Flex
         ref={ref}
         forwardedAs={as}
@@ -37,14 +37,12 @@ const Flex = intrinsicComponent<FlexProps, FlexElement>(function Flex(
 
 Flex.propTypes = {
   ...extract([Block]),
-  ...taggable(objectValues(FlexContainer)),
 
   direction: PT.oneOf(objectValues(DirectionType)),
 
   isReversed: PT.bool,
   hasWrap: PT.bool,
 
-  ...taggable(objectValues(FlexContainer)),
   ...alignable(),
   ...stylableComponent(),
 } as WeakValidationMap<FlexProps>;
