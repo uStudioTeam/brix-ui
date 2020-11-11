ae1ae59d84813b49058d2d2bf17493c877007f74
drizzer14 <drizzer14@gmail.com>
2020-11-11 10:49:51 +0200

    fix(Badge): parse `color` and `backgroundColor` from props

--- packages/core/src/badge/badge.styles.ts
+++ packages/core/src/badge/badge.styles.ts
@@ -37,8 +37,8 @@ const Badge = styled.div<
     verticalOffset,
     theme,
   }) => {
-    const backgroundColor = $backgroundColor || theme.palette[Color.FaintWeak];
-    const color = $color || theme.colorHelper.getContrastingColor(backgroundColor);
+    const backgroundColor = theme.colorHelper.parseColor($backgroundColor) || theme.palette[Color.FaintWeak];
+    const color = theme.colorHelper.parseColor($color) || theme.colorHelper.getContrastingColor(backgroundColor);
 
     return css`
       display: flex;
