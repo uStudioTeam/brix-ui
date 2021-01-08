4d59e798f404983e146790c26967348b5b5af82b
drizzer14 <drizzer14@gmail.com>
2021-01-08 17:15:14 +0200

    fix(Block): remove `TextTag` type exclusion from the props extension of `Taggable`

--- packages/core/src/block/block.props.d.ts
+++ packages/core/src/block/block.props.d.ts
@@ -1,13 +1,13 @@
 import type { HTMLAttributes, PropsWithChildren } from 'react';
 
 import type { Gapable, Indent, IntrinsicComponent, StylableComponent, Taggable } from '@brix-ui/types/component';
-import type { HtmlTag, TextTag } from '@brix-ui/types/html';
+import type { HtmlTag } from '@brix-ui/types/html';
 
 export interface BlockProps
   extends PropsWithChildren<IntrinsicComponent<HTMLAttributes<HTMLElement>>>,
     StylableComponent,
     Gapable,
-    Taggable<Exclude<HtmlTag, TextTag>> {
+    Taggable<HtmlTag> {
   isInline?: boolean;
 
   margin?: Indent;
