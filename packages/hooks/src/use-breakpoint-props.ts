dc42702c03a96653a0feff47451bc3867a840c75
drizzer14 <drizzer14@gmail.com>
2021-01-11 12:16:16 +0200

    feat(hooks): update and simplify `useBreakpointProps` function type

--- packages/hooks/src/use-breakpoint-props.ts
+++ packages/hooks/src/use-breakpoint-props.ts
@@ -5,16 +5,18 @@ import { spread } from '@brix-ui/utils/functions';
 
 import useMediaQuery from './use-media-query';
 
-// @TODO [Dmytro Vasylkivskyi]: fix typing
+type Breakpoint = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+type WithCurrentBreakpoint<T = {}> = With<T, { currentBreakpoint: number }>;
+
 /**
  * Merges props from all the breakpoints up to the currently matching one.
  * Expects `xs` to be the default
  */
-export default function useBreakpointProps<
-  R extends With<Record<string, unknown>, { currentBreakpoint: number }>,
-  A extends Omit<R, 'currentBreakpoint'>,
-  B extends R
->({ sm, md, lg, xl, ...xs }: A, breakpoints: Record<'xs' | 'sm' | 'md' | 'lg' | 'xl', number>): R {
+export default function useBreakpointProps<T extends object | undefined>(
+  props: T & Record<Exclude<Breakpoint, 'xs'>, T | undefined>,
+  breakpoints: Record<Breakpoint, number>
+): WithCurrentBreakpoint<T> {
+  const { sm, md, lg, xl, ...xs } = props;
   const { xs: bpXs, sm: bpSm, md: bpMd, lg: bpLg, xl: bpXl } = breakpoints;
 
   const isSm = useMediaQuery(`screen and (min-width: ${bpSm}px)`);
@@ -22,23 +24,23 @@ export default function useBreakpointProps<
   const isLg = useMediaQuery(`screen and (min-width: ${bpLg}px)`);
   const isXl = useMediaQuery(`screen and (min-width: ${bpXl}px)`);
 
-  return useMemo(() => {
+  return useMemo<WithCurrentBreakpoint<T>>(() => {
     if (isXl) {
-      return spread({ currentBreakpoint: bpXl }, xs as B, ...([sm, md, lg, xl] as B[])) as R;
+      return spread({ currentBreakpoint: bpXl }, xs as T, ...([sm, md, lg, xl] as T[]));
     }
 
     if (isLg) {
-      return spread({ currentBreakpoint: bpLg }, xs as B, ...([sm, md, lg] as B[])) as R;
+      return spread({ currentBreakpoint: bpLg }, xs as T, ...([sm, md, lg] as T[]));
     }
 
     if (isMd) {
-      return spread({ currentBreakpoint: bpMd }, xs as B, ...([sm, md] as B[])) as R;
+      return spread({ currentBreakpoint: bpMd }, xs as T, ...([sm, md] as T[]));
     }
 
     if (isSm) {
-      return spread({ currentBreakpoint: bpSm }, xs as B, sm as B) as R;
+      return spread({ currentBreakpoint: bpSm }, xs as T, sm as T);
     }
 
-    return spread({ currentBreakpoint: bpXs }, xs as B) as B;
+    return spread({ currentBreakpoint: bpXs }, xs);
   }, [xs, isSm, sm, isMd, md, isLg, lg, isXl, xl]);
 }
